8ecc57eba8e9972723db83d414a06130772e7171
“ktym4a” <s19585574@gmail.com>
2020-12-24 11:56:33 +0700

    fix Meet the Node console

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
-// var express = require('express');
-// var app = express();
+var express = require('express');
+var app = express();
 
 
 console.log("Hello World")
@@ -36,4 +36,4 @@ console.log("Hello World")
 
 
 
-// module.exports = app;
+module.exports = app;
